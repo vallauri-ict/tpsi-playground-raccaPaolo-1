66491cd46608eca2bd1c22c00d46aed5ccb934a2
Paolo Racca <p.racca.1368@vallauri.edu>
2021-11-17 09:26:26 +0100

    [Ex5_013] Finished ex

--- 5_013_Aggregate/server.ts
+++ 5_013_Aggregate/server.ts
@@ -119,3 +119,162 @@ mongoClient.connect(CONNECTION_STRING, (err, client) => {
     console.log("Errore connessione al db: " + err.message);
   }
 });
+
+//  query 6
+mongoClient.connect(CONNECTION_STRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DB_NAME);
+    db.collection("unicorns")
+      .aggregate([
+        {
+          $group: {
+            _id: { gender: {} },
+            media: { $avg: "$vampires" },
+          },
+        },
+        { $project: { _id: 0, media_arr: { $round: "$media" } } },
+      ])
+      .toArray()
+      .then((data) => console.log("Query 6", data))
+      .catch((err) => console.log("Errore esecuzione query: " + err.message))
+      .finally(() => client.close());
+  } else {
+    console.log("Errore connessione al db: " + err.message);
+  }
+});
+
+//  query 7
+mongoClient.connect(CONNECTION_STRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DB_NAME);
+    db.collection("quizzes")
+      .aggregate([
+        {
+          $project: {
+            quizAvg: { $avg: "$quizzes" },
+            labAvg: { $avg: "$labs" },
+            examAvg: { $avg: ["$midterm", "$final"] },
+          },
+        },
+        {
+          $project: {
+            quizAvg: { $round: ["$quizAvg", 1] },
+            labAvg: { $round: ["$labAvg", 1] },
+            examAvg: { $round: ["$examAvg", 1] },
+          },
+        },
+        {
+          $group: {
+            _id: {},
+            mediaQuizAvg: { $avg: "$quizAvg" },
+            mediaLabAvg: { $avg: "$labAvg" },
+            mediaExamAvg: { $avg: "$examAvg" },
+          },
+        },
+        {
+          $project: {
+            mediaQuizAvg: { $round: ["$mediaQuizAvg", 2] },
+            mediaLabAvg: { $round: ["$mediaLabAvg", 2] },
+            mediaExamAvg: { $round: ["$mediaExamAvg", 2] },
+          },
+        },
+      ])
+      .toArray()
+      .then((data) => console.log("Query 7", data))
+      .catch((err) => console.log("Errore esecuzione query: " + err.message))
+      .finally(() => client.close());
+  } else {
+    console.log("Errore connessione al db: " + err.message);
+  }
+});
+
+//  query 8
+mongoClient.connect(CONNECTION_STRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DB_NAME);
+    let regex = new RegExp("F", "i");
+    db.collection("students")
+      .aggregate([
+        {
+          $match: {
+            genere: { $regex: regex },
+          },
+        },
+        {
+          $project: {
+            nome: 1,
+            mediaVoti: { $avg: "$voti" },
+            //  id portato avanti per default
+          },
+        },
+        {
+          $sort: {
+            mediaVoti: -1,
+          },
+        },
+        {
+          $skip: 1,
+        },
+        {
+          $limit: 1,
+        },
+      ])
+      .toArray()
+      .then((data) => console.log("Query 8", data))
+      .catch((err) => console.log("Errore esecuzione query: " + err.message))
+      .finally(() => client.close());
+  } else {
+    console.log("Errore connessione al db: " + err.message);
+  }
+});
+
+//  query 9
+mongoClient.connect(CONNECTION_STRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DB_NAME);
+    db.collection("orders")
+      .aggregate([
+        {
+          $project: {
+            status: 1,
+            nDettagli: 1,
+          },
+        },
+        {
+          $unwind: "$nDettagli",
+        },
+        {
+          $group: {
+            _id: "$status",
+            elementi: { $sum: "$nDettagli" },
+          },
+        },
+      ])
+      .toArray()
+      .then((data) => console.log("Query 9", data))
+      .catch((err) => console.log("Errore esecuzione query: " + err.message))
+      .finally(() => client.close());
+  } else {
+    console.log("Errore connessione al db: " + err.message);
+  }
+});
+
+//  query 10
+mongoClient.connect(CONNECTION_STRING, (err, client) => {
+  if (!err) {
+    let db = client.db(DB_NAME);
+    //  se posso usare il find e non è il caso di usare l'aggregate meglio, più veloce
+    db.collection("students")
+      .find({
+        $expr: {
+          $gte: [{ $year: "$nato" }, 2000],
+        },
+      })
+      .toArray()
+      .then((data) => console.log("Query 9", data))
+      .catch((err) => console.log("Errore esecuzione query: " + err.message))
+      .finally(() => client.close());
+  } else {
+    console.log("Errore connessione al db: " + err.message);
+  }
+});
