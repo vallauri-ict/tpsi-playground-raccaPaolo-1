8d9b22b86d01b6109d6c67a7fdcd1a7e50c4cc55
Paolo Racca <p.racca.1368@vallauri.edu>
2022-02-10 10:27:47 +0100

    [5_018] Finish exercise

--- 03_express/5_018_WebSocket/static/index.js
+++ 03_express/5_018_WebSocket/static/index.js
@@ -1,55 +1,77 @@
 $(document).ready(function () {
   let user = { username: "", room: "" };
 
+  let btnConnetti = $("#btnConnetti");
+  let btnDisconnetti = $("#btnDisonnetti");
+  let btnInvia = $("#btnInvia");
+
+  btnDisconnetti.prop("disabled", true);
+  btnInvia.prop("disabled", true);
+
   // mi connetto al server che mi ha inviato la pagina,
   // il quale mi restituisce il suo serverSocket
   // io.connect é SINCRONO, bloccante
-  let serverSocket = io({
-    transports: ["websocket"],
-    upgrade: false,
-  }).connect();
-
-  /* 1a) lo username viene inviato SOLO a connessione avvenuta
-	       in questo modo si evita di connetere/disconnettere + volte */
-  serverSocket.on("connect", function () {
-    console.log("connessione ok");
-    user.username = prompt("Inserisci lo username:");
-    if (user.username === "pippo" || user.username === "pluto")
-      user.room = "room1";
-    else user.room = "defaultRoom";
-    serverSocket.emit("login", JSON.stringify(user));
-  });
-
-  // 1b) utente valido / non valido
-  serverSocket.on("loginAck", function (data) {
-    if (data == "NOK") {
-      alert("Nome già esistente. Scegliere un altro nome");
-      user.username = prompt("Inserisci un nuovo username:");
-      serverSocket.emit("login", user.username);
-    } else document.title = user.username;
-  });
+  let serverSocket;
 
   // 2a) invio messaggio
-  $("#btnInvia").click(function () {
+  btnInvia.click(function () {
     let msg = $("#txtMessage").val();
     serverSocket.emit("message", msg);
-  });
-
-  // 2b) ricezione della risposta
-  serverSocket.on("message_notify", function (data) {
-    // ricezione di un messaggio dal server
-    data = JSON.parse(data);
-    visualizza(data.from, data.message, data.date);
+    $("#txtMessage").val("");
   });
 
   // 3) disconnessione
   $("#btnDisconnetti").click(function () {
     serverSocket.disconnect();
+    btnConnetti.prop("disabled", false);
+    btnInvia.prop("disabled", true);
   });
 
-  serverSocket.on("disconnect", function () {
-    alert("Sei stato disconnesso!");
+  btnConnetti.on("click", () => {
+    btnConnetti.prop("disabled", true);
+    btnDisconnetti.prop("disabled", false);
+    btnInvia.prop("disabled", false);
+    serverSocket ==
+      io({
+        transports: ["websocket"],
+        upgrade: false,
+      }).connect();
+
+    /* 1a) lo username viene inviato SOLO a connessione avvenuta
+	       in questo modo si evita di connetere/disconnettere + volte */
+    serverSocket.on("connect", function () {
+      console.log("connessione ok");
+      setUser();
+      serverSocket.emit("login", JSON.stringify(user));
+    });
+
+    // 1b) utente valido / non valido
+    serverSocket.on("loginAck", function (data) {
+      if (data == "NOK") {
+        alert("Nome già esistente. Scegliere un altro nome");
+        setUser();
+        serverSocket.emit("login", JSON.stringify(user));
+      } else document.title = user.username;
+    });
+
+    // 2b) ricezione della risposta
+    serverSocket.on("message_notify", function (data) {
+      // ricezione di un messaggio dal server
+      data = JSON.parse(data);
+      visualizza(data.from, data.message, data.date);
+    });
+
+    serverSocket.on("disconnect", function () {
+      alert("Sei stato disconnesso!");
+    });
   });
+
+  function setUser() {
+    user.username = prompt("Inserisci lo username:");
+    if (user.username === "pippo" || user.username === "pluto")
+      user.room = "room1";
+    else user.room = "defaultRoom";
+  }
 
   function visualizza(username, message, date) {
     let wrapper = $("#wrapper");
